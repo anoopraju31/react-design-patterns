858a42992dc1a6ed75ae7e5946f4db65ad6c9724
anoopraju31 <anoopraju31@gmail.com>
2024-06-29 10:17:30 +0530

    Tailwind CSS added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { FC } from 'react'
 
 const App: FC = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		</div>
+	)
 }
 
 export default App
